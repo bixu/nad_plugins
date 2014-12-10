4e53da85b066e2fbf51684173e509c683ed5dc68
Blake Irvin <bixv@icloud.com>
2014-12-10 14:01:16 +0100

    simpler

--- node-agent.d/smartos/cpu_cap.sh
+++ node-agent.d/smartos/cpu_cap.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-set -e
-
-CPU_CAP_USED=$(sm-cpuinfo | grep "Cap Used" | cut -d" " -f4)
-
-PREFIX="cpu_cap"
-
-echo -e "${PREFIX}:used_percent\tn\t${CPU_CAP_USED}"
+echo -e "used_percent\tn\t$(sm-cpuinfo | grep "Cap Used" | cut -d" " -f4)"
